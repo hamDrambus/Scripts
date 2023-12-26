b37725f72d939947caf849a17c5e4512c807a8b5
hamDrambus <geffdroid@gmail.com>
2023-12-26 13:51:07 +0700

    --

--- my_startup.sh
+++ my_startup.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-#xinput --set-prop "YSPRINGTECH USB OPTICAL MOUSE" "Device Accel Velocity Scaling" 10.0
-#xinput --set-prop "YSPRINGTECH USB OPTICAL MOUSE" "Device Accel Constant Deceleration" 1.5
-#xinput --set-prop "YSPRINGTECH USB OPTICAL MOUSE" "Device Accel Adaptive Deceleration" 1.5 
-source /home/frolov/Software/root_v6.14.06/build/bin/thisroot.sh
-#export PATH=$PATH:/usr/lib64/qt4/bin/
-#
-export GARFIELD_HOME=/home/frolov/Software/GarfieldPP/
-export HEED_DATABASE=$GARFIELD_HOME/Heed/heed++/database
+source /home/frolov/Software/root_v6.28.06/build/bin/thisroot.sh
+source /home/frolov/Software/Geant4/geant4-v11.0.0-install/bin/geant4.sh
 
-export ELMER_HOME=/home/Frolov/Documents/Elmer/install
+#
+export GARFIELD_HOME=/home/frolov/Software/GarfieldPP_2022/install
+export HEED_DATABASE=$GARFIELD_HOME/share/Heed/database
+
+export ELMER_HOME=/home/frolov/Software/Elmer/install
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ELMER_HOME/lib
 #LD_LIBRARY_PATH cannot be set in etc/profile.d/
 #see Ubuntu_readme.txt
-export PATH=$PATH:$ELMER_HOME/bin
+export PATH=$PATH:$ELMER_HOME/bin:/home/frolov/.local/bin
 
 touch /home/frolov/Documents/profile.d.test
-echo "PATH=$PATH" >> /home/frolov/Documents/profile.d.test
+echo "PATH=$PATH" > /home/frolov/Documents/profile.d.test
+
+alias python="python3"
+
